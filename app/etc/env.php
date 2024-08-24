a527ee26fc15adc55657d7b381c8e6db59a2df09
Minh Nhat <nhattdvp211202@gmail.com>
2024-08-24 17:59:35 +0700

    Initial commit

---
+++ app/etc/env.php
@@ -0,0 +1,89 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'cF3QCiGi4cQiA07QGoQGe6pmpru80bHq'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'cf7_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'cf7_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'config' => [
+        'async' => 0
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'base641dUnMAYg6dpj+kWHfay4FzuCavhUsXMpK9QhidhKgnk='
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2',
+                'username' => 'magento2',
+                'password' => 'Nhat2002@',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'graphql_query_resolver_result' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'localhost.magento2.com'
+    ],
+    'install' => [
+        'date' => 'Fri, 23 Aug 2024 15:37:59 +0000'
+    ]
+];
